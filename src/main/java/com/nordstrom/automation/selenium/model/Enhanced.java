ba339eddc09dd3cde417a3ea79e5ea03bd35a4e7
Scott Babcock <scoba@hotmail.com>
2018-01-27 18:04:32 -0800

    Retire this short-term variant

--- src/main/java/com/nordstrom/automation/selenium/model/Enhanced.java
+++
@@ -1,6 +0,0 @@
-package com.nordstrom.automation.selenium.model;
-
-/**
- * This marker interface is added to dynamically created classes for future identification
- */
-public interface Enhanced { }
--- src/main/resources/locateByXpath.js
+++
@@ -1,2 +0,0 @@
-var context = (arguments[0].length) ? arguments[0][0] : document;
-return document.evaluate(arguments[1], context, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
